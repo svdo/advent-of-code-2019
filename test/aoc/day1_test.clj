85eb7eed5f22d2e17e2d1ff7792ae8bce7873d9f
Stefan van den Oord <stefan.van.den.oord@philips.com>
2019-12-03 13:07:27 +0100

    Adds total-fuel-requirement and solves part a

--- test/aoc/day1_test.clj
+++ test/aoc/day1_test.clj
@@ -1,7 +1,15 @@
 (ns aoc.day1-test
   (:require [clojure.test :refer [deftest testing is]]
-            [aoc.day1 :refer [calculate-fuel]]))
+            [aoc.day1 :refer [input calculate-fuel total-fuel-requirement]]))
 
 (deftest day1-test
   (testing "Fuel calculation"
-    (is (= 2 (calculate-fuel 12)))))
+    (is (= 2 (calculate-fuel 12)))
+    (is (= 2 (calculate-fuel 14)))
+    (is (= 654 (calculate-fuel 1969)))
+    (is (= 33583 (calculate-fuel 100756))))
+
+  (testing "Total fuel requirement"
+    (is (= 4 (total-fuel-requirement [12 14])))
+    (is (= 658 (total-fuel-requirement [12 14 1969])))
+    (is (= 3538016 (total-fuel-requirement input)))))
